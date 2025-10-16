30c4fe188b2ca02681686507ed076cb3cbf849f5
Selvin93153 <Selvin.franco@outlook.com>
2025-10-15 23:43:11 -0600

    se modifica la restrinccion de roles en el endpoint para filtrar por id

--- src/movimientos_equipos/movimientos-equipos.controller.ts
+++ src/movimientos_equipos/movimientos-equipos.controller.ts
@@ -5,46 +5,53 @@ import { UpdateMovimientoEquipoDto } from './dto/update-movimiento-equipo.dto';
 import { RolesGuard } from 'src/common/guards/roles.guard';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { Roles } from 'src/common/decorators/roles.decorator';
+import { Public } from 'src/common/decorators/public.decorator';
 
 @Controller('movimientos-equipos')
 @UseGuards(JwtAuthGuard, RolesGuard)
-@Roles('armero')
 export class MovimientosEquiposController {
   constructor(private readonly service: MovimientosEquiposService) {}
 
   @Post()
+  @Roles('armero')
   create(@Body() dto: CreateMovimientoEquipoDto) {
     return this.service.create(dto);
   }
 
   @Get()
+   @Roles('armero')
   findAll() {
     return this.service.findAll();
   }
 
   // Filtrar datos po nip
 @Get('por-nip/:nip')
+ @Roles('armero')
 async findByNip(@Param('nip') nip: string) {
   return this.service.findByNipRecibe(nip);
 }
 
 @Get('prestamos-en-uso/:id_usuario')
+ @Roles('armero', 'jefe', 'agente operativo')
 async getPrestamosEnUso(@Param('id_usuario') id_usuario: number) {
   return this.service.findPrestamosEnUsoPorUsuario(+id_usuario);
 }
 
 
   @Get(':id')
+    @Public()
   findOne(@Param('id') id: number) {
     return this.service.findOne(+id);
   }
 
   @Patch(':id')
+   @Roles('armero')
   update(@Param('id') id: number, @Body() dto: UpdateMovimientoEquipoDto) {
     return this.service.update(+id, dto);
   }
 
   @Delete(':id')
+   @Roles('armero')
   remove(@Param('id') id: number) {
     return this.service.remove(+id);
   }
